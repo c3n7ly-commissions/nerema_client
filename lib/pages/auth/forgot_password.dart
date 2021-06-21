7f7e3646af9c3b4a035c4a17ff77786862d9bca4
Timothy Karani <kartimothy@gmail.com>
2021-06-21 15:48:36 +0300

    Moved pages to own files

---
+++ lib/pages/auth/forgot_password.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forgot Password"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/sign-in');
+          },
+          child: Text('Sign In'),
+        ),
+      ),
+    );
+  }
+}
