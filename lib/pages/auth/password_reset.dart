54d26d2f83256aec8282181562debc7b99369609
Timothy Karani <kartimothy@gmail.com>
2021-06-21 16:00:30 +0300

    The migration begun

--- lib/pages/auth/password_reset.dart
+++ lib/pages/auth/password_reset.dart
@@ -1,18 +1,201 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
 
-class PasswordResetScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PasswordResetScreen extends StatefulWidget {
+  @override
+  _PasswordResetScreenState createState() => _PasswordResetScreenState();
+}
+
+class _PasswordResetScreenState extends State<PasswordResetScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Forgot Password"),
-      ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/sign-in');
-          },
-          child: Text('Sign In'),
+        child: Card(
+          child: Padding(
+            padding: EdgeInsets.all(18.0),
+            child: SignInForm(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignInForm extends StatefulWidget {
+  @override
+  _SignInFormState createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future signIn(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8000/api/v1/dj-rest-auth/login/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'username': email,
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // Successful request
+      final Map<String, dynamic> token = jsonDecode(response.body);
+      print("${response.statusCode} : ${response.body}");
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Row(
+            children: <Widget>[
+              Text(
+                "Success: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text('Logged in successfully ${token["key"]}'),
+            ],
+          ),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } else if (response.statusCode == 400) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Row(
+            children: <Widget>[
+              Text(
+                "Error: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text('Invalid credentials'),
+            ],
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } else {
+      print("${response.statusCode} : ${response.body}");
+    }
+  }
+
+  _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      signIn(_emailController.text, _passwordController.text);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Container(
+        width: 500,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(bottom: 15.0),
+              child: Center(
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 50,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0),
+              child: TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'E-mail',
+                  labelText: 'E-mail*',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your e-mail";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0),
+              child: TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                  labelText: 'Password*',
+                ),
+                obscureText: true,
+                autocorrect: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your password";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0),
+              child: Text(
+                'Fields marked with * sign are required',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0),
+              child: Container(
+                width: double.infinity,
+                child: TextButton(
+                  child: Text('Sign In'),
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.blue,
+                    padding: EdgeInsets.symmetric(vertical: 20.0),
+                  ),
+                  onPressed: _submitForm,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Center(
+                child: TextButton(
+                  child: Text('Forgot password'),
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.all(20.0),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/forgot-password');
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
